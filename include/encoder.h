5de74b9ee1360b3f740658467cd15323c5c679d5
Blake Sheridan <bsheridan@harttech.com>
2013-05-01 17:51:46 -0400

    encoder.h: prototypes just for external stuff

--- include/encoder.h
+++ include/encoder.h
@@ -24,31 +24,4 @@ typedef struct {
 
 } Encoder;
 
-static PyObject* encode                     (Encoder *self, PyObject *o);
-static PyObject* encode_bytes               (Encoder *self, PyObject *o);
-
-static int           _append                (Encoder *self, PyObject *o);
-Py_LOCAL_INLINE(int) _append_int            (Encoder *self, PyObject *py_int);
-Py_LOCAL_INLINE(int) _append_float          (Encoder *self, PyObject *py_float);
-
-Py_LOCAL_INLINE(int) _append_str            (Encoder *self, PyObject *str);
-Py_LOCAL_INLINE(int) _append_str_1byte_kind (Encoder *self, PyObject *str, int length);
-Py_LOCAL_INLINE(int) _append_str_2byte_kind (Encoder *self, PyObject *str, int length);
-Py_LOCAL_INLINE(int) _append_str_4byte_kind (Encoder *self, PyObject *str, int length);
-Py_LOCAL_INLINE(int) _append_str_naive      (Encoder *self, PyObject *str, int length);
-
-Py_LOCAL_INLINE(int) _append_bytes          (Encoder *self, PyObject *bytes);
-Py_LOCAL_INLINE(int) _append_dict           (Encoder *self, PyObject *dict);
-Py_LOCAL_INLINE(int) _append_fast_sequence  (Encoder *self, PyObject *list_or_tuple);
-Py_LOCAL_INLINE(int) _append_mapping        (Encoder *self, PyObject *mapping);
-
-/*
- * Lazy initialization accessors.
- * Should be the only means of getting their respective attributes.
- */
-Py_LOCAL_INLINE(PyObject*)  _get_str_translation_table (Encoder *self);
-Py_LOCAL_INLINE(Py_UCS1 **) _get_str_ucs1_mapping      (Encoder *self);
-
-static void _xfree_str_ucs1_mapping(Encoder *self);
-
 #endif
--- src/encoder.c
+++ src/encoder.c
@@ -3,6 +3,35 @@
 #include "buffer.h"
 #include "encoder.h"
 
+/* Forward declarations */
+static PyObject* encode                     (Encoder *self, PyObject *o);
+static PyObject* encode_bytes               (Encoder *self, PyObject *o);
+
+static int           _append                (Encoder *self, PyObject *o);
+Py_LOCAL_INLINE(int) _append_bytes          (Encoder *self, PyObject *bytes);
+Py_LOCAL_INLINE(int) _append_dict           (Encoder *self, PyObject *dict);
+Py_LOCAL_INLINE(int) _append_int            (Encoder *self, PyObject *py_int);
+Py_LOCAL_INLINE(int) _append_fast_sequence  (Encoder *self, PyObject *list_or_tuple);
+Py_LOCAL_INLINE(int) _append_float          (Encoder *self, PyObject *py_float);
+Py_LOCAL_INLINE(int) _append_mapping        (Encoder *self, PyObject *mapping);
+
+Py_LOCAL_INLINE(int) _append_str            (Encoder *self, PyObject *str);
+Py_LOCAL_INLINE(int) _append_str_1byte_kind (Encoder *self, PyObject *str, int length);
+Py_LOCAL_INLINE(int) _append_str_2byte_kind (Encoder *self, PyObject *str, int length);
+Py_LOCAL_INLINE(int) _append_str_4byte_kind (Encoder *self, PyObject *str, int length);
+Py_LOCAL_INLINE(int) _append_str_naive      (Encoder *self, PyObject *str, int length);
+
+Py_LOCAL_INLINE(int) _append_bytes_constant (Encoder *self, PyObject **member, const char *attribute_name);
+
+/*
+ * Lazy initialization accessors.
+ * Should be the only means of getting their respective attributes.
+ */
+Py_LOCAL_INLINE(PyObject*)  _get_str_translation_table (Encoder *self);
+Py_LOCAL_INLINE(Py_UCS1 **) _get_str_ucs1_mapping      (Encoder *self);
+
+static void _xfree_str_ucs1_mapping(Encoder *self);
+
 PyDoc_STRVAR(__doc__,
 "TODO Encoder __doc__");
 
@@ -11,9 +40,6 @@ PyDoc_STRVAR(encode___doc__,
 
 PyDoc_STRVAR(encode_bytes___doc__,
 "TODO encode_bytes __doc__");
-
-/* Forward declarations */
-Py_LOCAL_INLINE(int) _append_bytes_constant(Encoder *self, PyObject **member, const char *attribute_name);
 
 static PyObject *
 __new__(PyTypeObject *type, PyObject *args, PyObject **kwargs)
